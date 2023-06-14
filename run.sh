4366634af90e9cb6f3391db9873d02efcfae22e5
aseef <contact@aseef.dev>
2023-06-13 23:45:37 -0400

    fixed run.sh perms

--- run.sh
+++ run.sh
@@ -2,10 +2,12 @@
 # this script is run by azure as soon as the app is deployed
 
 # install npm dependencies
+echo "Installing NPM dependencies..."
 cd ./frontend || exit
 npm install
 cd ../backend || exit
 npm install
 cd ../
+echo "Success! NPM dependencies are now installed. Starting the app..."
 # call pm2 to start the node server
 pm2 start ecosystem.config.js --no-daemon
