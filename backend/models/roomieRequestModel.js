cf328cb9a1bb803d80a8fb2f9d5a8b97eafc3a93
aseef <contact@aseef.dev>
2023-06-14 16:13:55 -0400

    mostly finished the roomie request models and also the status model

--- backend/models/roomieRequestModel.js
+++ backend/models/roomieRequestModel.js
@@ -1,14 +1,46 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const roomieReqSchema = new Schema({
-    id: {
+const HousingInfoSchema = new Schema({
+    address: {
+        type: String,
+        required: true
+    },
+    longitude: {
         type: Number,
         required: true
     },
+    latitude: {
+        type: Number,
+        required: true
+    }
+})
+
+const RoomieRequestSchema = new Schema({
+    targetSemester: {
+        type: String,
+        required: true
+    },
+    numberOfRoomies: {
+        type: Number,
+        required: true
+    },
+    // only exists if the person has existing housing arrangements
+    housingInfo: {
+        type: HousingInfoSchema,
+        required: false
+    },
+    expiry: {
+        type: Number, //epoch date
+        required: true
+    },
+    currentStatus: {
+        type: Number, // 1=open, 2=pending, 3=completed
+        required: true
+    }
 }, {
     timestamps: true,
     autoIndex: true
 })
 
-module.exports = mongoose.model('RoomieRequest', roomieReqSchema)
+module.exports = mongoose.model('RoomieRequest', RoomieRequestSchema)
