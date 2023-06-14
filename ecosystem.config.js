fca10b00a035d3ba0088d69b513ad7757f0838ac
aseef <contact@aseef.dev>
2023-06-13 22:19:40 -0400

    Added a self-hosted gitlab runner in hopes of a faster build time

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,5 @@
 // This file is how Azure knows where the entry point for the node server is
+// See https://learn.microsoft.com/en-us/azure/app-service/configure-language-nodejs?pivots=platform-linux#run-with-pm2 for details
 module.exports = {
     apps : [{
         name   : "Roomie4Me",
