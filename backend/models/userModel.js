98e1b278b3f2d0df05c55f281bcbe35182cc105f
aseef <contact@aseef.dev>
2023-06-17 16:37:14 -0400

    fixed issues with the user schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,165 +1,140 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const NameSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-});
-
-const ReligiousAffiliationSchema = new Schema({
-  religion: {
-    type: String,
-    required: true,
-  },
-  scale: {
-    type: Number,
-    required: true,
-  },
-});
-
-const InternationStatusSchema = new Schema({
-  international: {
-    type: Boolean,
-    required: true,
-  },
-  country: {
-    type: String,
-    required: true,
-  },
-});
-
-const DegreeProgramSchema = new Schema({
-  majors: {
-    type: Array,
-    required: true,
-  },
-  minors: {
-    type: Array,
-    required: true,
-  },
-  graduation: {
-    type: Date,
-    required: true,
-  },
-});
-
-const DrugUseSchema = new Schema({
-  smoking: {
-    type: Number, // 4=often, 3=sometimes, 2=rarely, 1=never
-    required: true,
-  },
-  vaping: {
-    type: Number, // 4=often, 3=sometimes, 2=rarely, 1=never
-    required: true,
-  },
-  alcohol: {
-    type: Number, // 4=often, 3=sometimes, 2=rarely, 1=never
-    required: true,
-  },
-  other: {
-    type: Number, // 4=often, 3=sometimes, 2=rarely, 1=never
-    required: true,
-  },
-});
-
-const SleepScheduleSchema = new Schema({
-  bedtime: {
-    type: Number,
-    required: true,
-  },
-  waketime: {
-    type: Number,
-    required: true,
-  },
-});
-
-const WeeklySleepScheduleSchema = new Schema({
-  weekdays: {
-    type: SleepScheduleSchema,
-    required: true,
-  },
-  weekends: {
-    type: SleepScheduleSchema,
-    required: true,
-  },
-});
-
 const UserSchema = new Schema(
-  {
-    openid: {
-      type: String,
-      required: true,
-      unique: true,
+    {
+        openid: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        registered: {
+            type: Boolean,
+            required: true,
+        },
+        name: {
+            firstName: {
+                type: String,
+                required: false,
+            },
+            lastName: {
+                type: String,
+                required: false,
+            },
+        },
+        age: {
+            type: Number,
+            required: false,
+        },
+        gender: {
+            type: String,
+            required: false,
+        },
+        languages: {
+            type: Array,
+            required: false,
+        },
+        religiousAffiliation: {
+            religion: {
+                type: String,
+                required: false,
+            },
+            scale: {
+                type: Number,
+                required: false,
+            },
+        },
+        internationalStatus: {
+            international: {
+                type: Boolean,
+                required: false,
+            },
+            country: {
+                type: String,
+                required: false,
+            },
+        },
+        degreeProgram: {
+            majors: {
+                type: Array,
+                required: false,
+            },
+            minors: {
+                type: Array,
+                required: false,
+            },
+            graduation: {
+                type: Date,
+                required: false,
+            },
+        },
+        drugs: {
+            smoking: {
+                type: Number, // 4=often, 3=sometimes, 2=rarely, 1=never
+                required: false,
+            },
+            vaping: {
+                type: Number, // 4=often, 3=sometimes, 2=rarely, 1=never
+                required: false,
+            },
+            alcohol: {
+                type: Number, // 4=often, 3=sometimes, 2=rarely, 1=never
+                required: false,
+            },
+            other: {
+                type: Number, // 4=often, 3=sometimes, 2=rarely, 1=never
+                required: false,
+            },
+        },
+        housing: {
+            hasHousing: {
+                type: Boolean,
+                required: false,
+            },
+            address: {
+                type: String,
+                required: false,
+            }
+        },
+        weeklySleepSchedule: {
+            weekdays: {
+                bedtime: {
+                    type: Date,
+                    required: false,
+                },
+                waketime: {
+                    type: Date,
+                    required: false,
+                },
+            },
+            weekends: {
+                bedtime: {
+                    type: Date,
+                    required: false,
+                },
+                waketime: {
+                    type: Date,
+                    required: false,
+                },
+            },
+        },
+        aboutMe: {
+            type: String,
+            required: false,
+        },
+        aboutMeTags: {
+            type: Array,
+            required: false,
+        },
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    registered: {
-      type: Boolean,
-      required: true,
-    },
-    name: {
-      firstName: {
-        type: String,
-        required: true,
-      },
-      lastName: {
-        type: String,
-        required: true,
-      },
-    },
-    age: {
-      type: Number,
-      required: false,
-    },
-    gender: {
-      type: String,
-      required: false,
-    },
-    languages: {
-      type: Array,
-      required: false,
-    },
-    religiousAffiliation: {
-      type: ReligiousAffiliationSchema,
-      required: false,
-    },
-    internationalStatus: {
-      type: InternationStatusSchema,
-      required: false,
-    },
-    degreeProgram: {
-      type: DegreeProgramSchema,
-      required: false,
-    },
-    drugs: {
-      type: DrugUseSchema,
-      required: false,
-    },
-    weeklySleepSchedule: {
-      type: WeeklySleepScheduleSchema,
-      required: false,
-    },
-    aboutMe: {
-      type: String,
-      required: false,
-    },
-    aboutMeTags: {
-      type: Array,
-      required: false,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 module.exports = mongoose.model("User", UserSchema);
