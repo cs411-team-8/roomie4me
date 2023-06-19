3bf1f8d87f5ca47f16e2d21ca5b445734a2a938b
munirsidd <munirsid@bu.edu>
2023-06-19 16:03:43 -0400

    updating

--- backend/routes/restapi/roomieRouter.js
+++ backend/routes/restapi/roomieRouter.js
@@ -1,85 +1,103 @@
-const express = require('express')
-const router = express.Router()
-const RoomieController = require('../../controller/roomieController')
+const express = require("express");
+const router = express.Router();
+const RoomieController = require("../../controller/roomieController");
 const tokenVerifier = require("../../utils/tokenVerifier");
 const UserController = require("../../controller/userController");
 
-router.post('/contactRequest', (req, res) => {
-    tokenVerifier.getAuthenticatedUser(req).then(user => {
-        UserController.contactRequest(req, res, user).catch(err => {
-            console.log(err)
-            res.status(500).send("An internal error occurred.")
-        })
-    }).catch(err => {
-        res.status(401).json({
-            error: "Unauthorized request or invalid access token."
-        })
+router.post("/contactRequest", (req, res) => {
+  tokenVerifier
+    .getAuthenticatedUser(req)
+    .then((user) => {
+      UserController.contactRequest(req, res, user).catch((err) => {
+        console.log(err);
+        res.status(500).send("An internal error occurred.");
+      });
     })
-})
+    .catch((err) => {
+      res.status(401).json({
+        error: "Unauthorized request or invalid access token.",
+      });
+    });
+});
 
-router.get('/requests', (req, res) => {
-    tokenVerifier.getAuthenticatedUser(req).then(user => {
-        RoomieController.getRequests(req, res, user).catch(err => {
-            console.log(err)
-            res.status(500).send("An internal error occurred.")
-        })
-    }).catch(err => {
-        res.status(401).json({
-            error: "Unauthorized request or invalid access token."
-        })
+router.get("/requests", (req, res) => {
+  tokenVerifier
+    .getAuthenticatedUser(req)
+    .then((user) => {
+      RoomieController.getRequests(req, res, user).catch((err) => {
+        console.log(err);
+        res.status(500).send("An internal error occurred.");
+      });
     })
-})
+    .catch((err) => {
+      res.status(401).json({
+        error: "Unauthorized request or invalid access token.",
+      });
+    });
+});
 
-router.get('/request', (req, res) => {
-    tokenVerifier.getAuthenticatedUser(req).then(user => {
-        RoomieController.getRequest(req, res, user).catch(err => {
-            console.log(err)
-            res.status(500).send("An internal error occurred.")
-        })
-    }).catch(err => {
-        res.status(401).json({
-            error: "Unauthorized request or invalid access token."
-        })
+router.get("/request", (req, res) => {
+  tokenVerifier
+    .getAuthenticatedUser(req)
+    .then((user) => {
+      RoomieController.getRequest(req, res, user).catch((err) => {
+        console.log(err);
+        res.status(500).send("An internal error occurred.");
+      });
     })
-})
+    .catch((err) => {
+      res.status(401).json({
+        error: "Unauthorized request or invalid access token.",
+      });
+    });
+});
 
-router.put('/create', (req, res) => {
-    tokenVerifier.getAuthenticatedUser(req).then(user => {
-        RoomieController.createRequest(req, res, user).catch(err => {
-            console.log(err)
-            res.status(500).send("An internal error occurred.")
-        })
-    }).catch(err => {
-        res.status(401).json({
-            error: "Unauthorized request or invalid access token."
-        })
+router.put("/create", (req, res) => {
+  tokenVerifier
+    .getAuthenticatedUser(req)
+    .then((user) => {
+      RoomieController.createRequest(req, res, user).catch((err) => {
+        console.log(err);
+        res.status(500).send("An internal error occurred.");
+      });
     })
-})
+    .catch((err) => {
+      res.status(401).json({
+        error: "Unauthorized request or invalid access token.",
+      });
+    });
+});
 
-router.post('/update', (req, res) => {
-    tokenVerifier.getAuthenticatedUser(req).then(user => {
-        RoomieController.updateRequest(req, res, user).catch(err => {
-            console.log(err)
-            res.status(500).send("An internal error occurred.")
-        })
-    }).catch(err => {
-        res.status(401).json({
-            error: "Unauthorized request or invalid access token."
-        })
+router.post("/update", (req, res) => {
+  tokenVerifier
+    .getAuthenticatedUser(req)
+    .then((user) => {
+      RoomieController.updateRequest(req, res, user).catch((err) => {
+        console.log(err);
+        res.status(500).send("An internal error occurred.");
+      });
     })
-})
+    .catch((err) => {
+      res.status(401).json({
+        error: "Unauthorized request or invalid access token.",
+      });
+    });
+});
 
-router.delete('/delete', (req, res) => {
-    tokenVerifier.getAuthenticatedUser(req).then(user => {
-        RoomieController.deleteRequest(req, res, user).catch(err => {
-            console.log(err)
-            res.status(500).send("An internal error occurred.")
-        })
-    }).catch(err => {
-        res.status(401).json({
-            error: "Unauthorized request or invalid access token."
-        })
+router.delete("/delete", (req, res) => {
+  tokenVerifier
+    .getAuthenticatedUser(req)
+    .then((user) => {
+      RoomieController.deleteRequest(req, res, user).catch((err) => {
+        console.log(err);
+        res.status(500).send("An internal error occurred.");
+      });
     })
-})
+    .catch((err) => {
+      res.status(401).json({
+        error: "Unauthorized request or invalid access token.",
+      });
+    });
+});
 
-module.exports = router
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,12 @@ conn.once("open", () => {
 // set json parser
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(bodyParser.json());
 app.use(cookieParser());
 // job when people send requests
