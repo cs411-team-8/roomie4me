e7c596c1354d9cc5c83aea7d7c148c057df3b301
munirsidd <munirsid@bu.edu>
2023-06-19 11:10:29 -0400

    Updated roomieRequestModel

--- backend/models/roomieRequestModel.js
+++ backend/models/roomieRequestModel.js
@@ -1,47 +1,62 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const RoomieRequestSchema = new Schema({
+const RoomieRequestSchema = new Schema(
+  {
     authorId: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     targetSemester: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     numberOfRoomies: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     // only exists if the person has existing housing arrangements
     housingInfo: {
-        address: {
-            type: String,
-            required: false
-        },
-        longitude: {
-            type: Number,
-            required: false
-        },
-        latitude: {
-            type: Number,
-            required: false
-        }
+      onCampus: {
+        type: Boolean,
+        required: false,
+      },
+      hasHousing: {
+        type: Boolean,
+        required: true,
+      },
+      address: {
+        type: String,
+        required: false,
+      },
+      longitude: {
+        type: Number,
+        required: false,
+      },
+      latitude: {
+        type: Number,
+        required: false,
+      },
+      desiredResidence: {
+        type: String,
+        required: true,
+      },
     },
     expiry: {
-        type: Number, //epoch date
-        required: true
+      type: Number, //epoch date
+      required: true,
     },
     currentStatus: {
-        type: Number, // 1=open, 2=pending, 3=completed
-        required: true
-    }
-}, {
+      type: Number, // 1=open, 2=pending, 3=completed
+      required: true,
+    },
+  },
+  {
     timestamps: true,
-    autoIndex: true
-})
+    autoIndex: true,
+  }
+);
 
-RoomieRequestSchema.index({ authorId: 1, targetSemester: 1}, { unique: true });
+RoomieRequestSchema.index({ authorId: 1, targetSemester: 1 }, { unique: true });
 
-module.exports = mongoose.model('RoomieRequest', RoomieRequestSchema)
+module.exports = mongoose.model("RoomieRequest", RoomieRequestSchema);
