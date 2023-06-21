3a7f90b28a8db41b480cfc57b516cd84c263e78d
munirsidd <munirsid@bu.edu>
2023-06-21 11:00:35 -0400

    updating

--- frontend/src/components/ViewRequests/ViewRequests.js
+++ frontend/src/components/ViewRequests/ViewRequests.js
@@ -175,15 +175,11 @@ function ViewRequests(props) {
                           marginBottom: "0px",
                         }}
                       >
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu
-                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit
-                        anim id est laborum.
+                        {`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in`.substring(
+                          0,
+                          450
+                        ) + " . . ."}
+                        {/* str.length <= 450 ? str : str.substring(0, 450) */}
                       </p>
                     </div>
                   </div>
